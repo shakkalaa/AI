5de15bd90d468202c356edc4e6a55dec18e69113
shakkalaa <85145974+shakkalaa@users.noreply.github.com>
2023-04-27 08:36:45 -0500

    Update Smosely.java

--- STATE SPACE PLANNER/planning/Example Planner/src/edu/uno/ai/planning/ex/Smosely.java
+++ STATE SPACE PLANNER/planning/Example Planner/src/edu/uno/ai/planning/ex/Smosely.java
@@ -8,7 +8,7 @@ import edu.uno.ai.planning.ss.StateSpaceSearch;
 /**
  * A planner that uses simple breadth first search through the space of states.
  * 
- * @author Your Name
+ * @author Shakayla Mosely
  */
 public class Smosely extends StateSpacePlanner {
 
@@ -39,4 +39,36 @@ public class Smosely extends StateSpacePlanner {
 		    //Let G = all the preconditions of the steps in S.
 		    //Recursively satisfy all the goals in G at level n – 1.
 		//If satisfying G fails, add a level to the graph and try again.
+	
+	
+	/* You see the weird “Atom” fields because Proposition itself is an abstract class that
+	several other classes implement. That said, in all of the domains and problems for this
+	project, you will only encounter two kinds of propositions: a literal by itself or a
+	conjunction of literals. You can use Java’s instanceof keyword to tell the difference
+	between the two.Here’s a helpful method to iterate through all the literals in a proposition:*/
+	
+	private static List<Literal> getLiterals(Proposition proposition) {
+		ArrayList<Literal> list = new ArrayList<>();
+		if(proposition instanceof Literal){
+			list.add((Literal) proposition);
+		}
+		else{
+			for(Proposition conjunct : ((Conjunction) proposition).arguments){
+				list.add((Literal) conjunct);
+			}
+		}
+		return list;
+	}
+	
+	//Alternatively, if you want to avoid allocating lists on the heap every time, you can use the Consumer interface to invoke a function on each literal:
+	private static void forEachLiteral(Proposition proposition, Consumer<? super Literal> consumer) {
+		if(proposition instanceof Literal){
+			consumer.accept((Literal) proposition);
+		}
+		else{
+			for(Proposition conjunct : ((Conjunction) proposition).arguments){
+				consumer.accept((Literal) conjunct);
+			}
+		}
+	}
 }
